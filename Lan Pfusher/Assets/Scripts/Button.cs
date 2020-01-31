da49b30f8f65964ddf6ddee2db29373a084f90c2
aston966 <aston966@gmail.com>
2020-02-01 00:22:11 +0100

    Creation Title_menu.unity

---
+++ Lan Pfusher/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Button : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+}
