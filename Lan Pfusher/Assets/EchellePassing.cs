5323b874aa9f46aa3e29ef5558b66e90cbd07c43
Altraya <contact@karakayn.com>
2020-02-01 03:13:37 +0100

    Continue script for passing to different floors

--- Lan Pfusher/Assets/EchellePassing.cs
+++ Lan Pfusher/Assets/EchellePassing.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Collider2D))]
 public class EchellePassing : MonoBehaviour
 {
+
+    //Destination dummy gameobject to be teleported on
+    public GameObject DestinationToTp;
+    //Reference to the camera to pass to it and allow player to see the good screen
+    public Camera DestinationCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,20 @@ public class EchellePassing : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //here the collider is normally the player
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if(col.gameObject.name == "Player"){
+            var dummyObjectDestPosition = DestinationToTp.gameObject.transform.position;
+            col.gameObject.transform.position = dummyObjectDestPosition;
+            //yield return new WaitForSeconds(3);
+            //Camera.current.gameObject.SetActive(false); //disable the previous scene camera
+            //DestinationCamera.gameObject.SetActive(true); //enable the current one
+        }
+        
+
     }
 }
