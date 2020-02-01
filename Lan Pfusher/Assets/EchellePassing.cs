fa542210c92b78e56cf2fd435c5c38cecf59cbfe
Altraya <contact@karakayn.com>
2020-02-01 11:05:36 +0100

    Small fix for character prefab and teleport

--- Lan Pfusher/Assets/EchellePassing.cs
+++ Lan Pfusher/Assets/EchellePassing.cs
@@ -8,6 +8,8 @@ public class EchellePassing : MonoBehaviour
 
     //Destination dummy gameobject to be teleported on
     public GameObject DestinationToTp;
+    //Reference to the camera to pass to it and allow player to see the good screen
+    public Camera DestinationCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,13 +26,18 @@ public class EchellePassing : MonoBehaviour
     {
         //here the collider is normally the player
         Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+
         if(col.gameObject.name == "Player"){
             var dummyObjectDestPosition = DestinationToTp.gameObject.transform.position;
             col.gameObject.transform.position = dummyObjectDestPosition;
-            var lastActiveCamera = Camera.current;
-            //lastActiveCamera.transform.position = dummyObjectDestPosition;
-          
+            var lastActiveCamera = Camera.current.gameObject;
+            
+            //yield return new WaitForSeconds(3);
+            //Camera.current.gameObject.SetActive(false); //disable the previous scene camera
+            //DestinationCamera.gameObject.SetActive(true); //enable the current one
         }
+        
+        
         
 
     }
