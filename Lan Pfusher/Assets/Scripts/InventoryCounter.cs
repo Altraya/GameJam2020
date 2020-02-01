dcd21112567c9dc9cfb5b740db517949d660a4a5
Nico-schwartz <nico.s67250@gmail.com>
2020-02-01 01:14:55 +0100

    added inventory on ui

---
+++ Lan Pfusher/Assets/Scripts/InventoryCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class InventoryCounter : MonoBehaviour
+{
+    public Item[] counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = new Item[5];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Text count = GetComponentInChildren(typeof(Text)) as Text;
+
+        if (count.name.ToUpper().Contains("COUNTER"))
+        {
+            count.text = counter.ToString();
+        }
+    }
+}
+
+public class Item
+{
+    public int counter;
+    public string name;
+}
