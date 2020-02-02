c2c1b69ac6c667575fb77ab0add204104db83a5a
aston966 <aston966@gmail.com>
2020-02-02 01:52:35 +0100

    menupause.cs fully functional

---
+++ Lan Pfusher/Assets/Scripts/RandomEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEventHandler : MonoBehaviour
+{
+
+    private List<GameObject> gamers;
+    private bool once;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            gamers = new List<GameObject>();
+            gamers.AddRange(GameObject.FindGameObjectsWithTag("GamerPNJ"));
+            once = true;
+
+        }
+        catch (System.Exception e)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(once)
+        {
+            foreach (GameObject go in gamers)
+            {
+                var gamerScript = go.GetComponent(typeof(GamerScript)) as GamerScript;
+                if (gamerScript.errorType == -1)
+                {
+                    gamerScript.RandomEvent();
+                }
+            }
+            once = false;
+        }
+    }
+}
---
+++ Lan Pfusher/Assets/Scripts/RepairScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairScript : MonoBehaviour
+{
+
+    private bool isRepairPossible;
+    GameObject currentGamer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentGamer != null)
+        {
+            GamerScript gs = (currentGamer.GetComponent(typeof(GamerScript)) as GamerScript);
+            int errorType = gs.errorType;
+            if (errorType >= 0)
+            {
+                if (Inventory.getInventoryQuantity(Inventory.getName(errorType)) > 0)
+                {
+                    if (Input.GetKey(KeyCode.Joystick1Button1))
+                    {
+                        gs.Repairing(true);
+                    }
+                    else
+                    {
+                        gs.Repairing(false);
+                    }
+                }
+                else
+                {
+                    gs.Repairing(false);
+                }
+            }
+            else
+            {
+                gs.Repairing(false);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("GamerPNJ"))
+        {
+            isRepairPossible = true;
+            currentGamer = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("GamerPNJ"))
+        {
+            isRepairPossible = false;
+        }
+    }
+
+}
