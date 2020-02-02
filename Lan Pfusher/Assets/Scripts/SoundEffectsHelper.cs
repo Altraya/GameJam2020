fa5fb62d6f422caaf125264922b1de3d33b689a4
Mickael67 <kistler.mickael@gmail.com>
2020-02-02 10:24:32 +0100

    Add non remove enum in inventory

---
+++ Lan Pfusher/Assets/Scripts/SoundEffectsHelper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Création d'effets sonores en toute simplicité
+/// </summary>
+public class SoundEffectsHelper : MonoBehaviour
+{
+
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static SoundEffectsHelper Instance;
+
+    public AudioClip SoundEffect_Non;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundEffectsHelper!");
+        }
+        Instance = this;
+    }
+
+    public void MakeSoundEffect_Non()
+    {
+        MakeSound(SoundEffect_Non);
+    }
+
+    /// <summary>
+    /// Lance la lecture d'un son
+    /// </summary>
+    /// <param name="originalClip"></param>
+    private void MakeSound(AudioClip originalClip)
+    {
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+}
